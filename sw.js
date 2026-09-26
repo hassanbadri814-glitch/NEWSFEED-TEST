59e5e9695e3984415af15b8a44ac56c9c747e429
Vatis <hassanbadri814@gmail.com>
2026-09-26 17:50:30 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
    - v2.6: HTML/JSON network-first, JS/CSS SWR, images cache-first
    ============================================================ */
 
-const CACHE_VERSION = 'v14.39';
+const CACHE_VERSION = 'v14.42';
 const STATIC_CACHE = 'wardesk-static-' + CACHE_VERSION;
 const MEDIA_CACHE = 'wardesk-media-v1';
 
